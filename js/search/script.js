02f8b29709d77c496fe08358665961bfeb75770f
MuskanBibhuty <muskanbibhuty@gmail.com>
2025-01-05 04:51:21 +0530

    api

--- js/search/script.js
+++ js/search/script.js
@@ -1,63 +1,35 @@
-const fetchTravelGuide = async (searchText) => {
-    const url = 'https://travel-guide-api-city-guide-top-places.p.rapidapi.com/check?noqueue=1';
+let res;
 
-    const options = {
-        method: 'POST',
-        headers: {
-            'x-rapidapi-key': 'b758d7a0demsh9fb25657fd959b7p1a5b67jsn9468655a271d',
-            'x-rapidapi-host': 'travel-guide-api-city-guide-top-places.p.rapidapi.com',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            region: searchText,
-            language: 'en',
-            interests: [
-                'historical',
-                'cultural',
-                'food'
-            ]
-        })
-    };
-
+const fetchTravelGuide = async (lat, long) => {
+    console.log(lat, long)
+    const url = `https://api.geoapify.com/v2/places?categories=tourism.sights&conditions=wheelchair&bias=proximity:${long},${lat}&limit=10&apiKey=9021f7ffcc134742bf20b2aacf7d632f`;
     try {
-        const response = await fetch(url, options);
+        const response = await fetch(url);
         const result = await response.json();
+        res = result;
         console.log(result);
     } catch (error) {
         console.error(error);
     }
-
-
-
-
-
-    // result.map((result) => {
-    //     const card = document.createElement('div')
-    //     card.classList.add("card")
-    //     const name = document.createElement('div')
-    //     name.classList.add("name")
-    //     name.textContent = result.name
-    //     const description = document.createElement('div')
-    //     description.classList.add("description")
-    //     description.textContent = result.description
-    //     const comment = document.createElement('div')
-    //     comment.classList.add("comment")
-    //     comment.textContent = result.comment
-    
-    //     card.appendChild(name)
-    //     card.appendChild(description)
-    //     card.appendChild(comment)
-    //     card.appendChild(card)
-    
-    // })
 };
 
+const GetLatlong = (place) => {
+    let geocoder = new google.maps.Geocoder();
+    let address = place;
+  
+    geocoder.geocode({
+      'address': address
+    }, function(results, status) {
 
+      if (status == google.maps.GeocoderStatus.OK) {
+        let latitude = results[0].geometry.location.lat();
+        let longitude = results[0].geometry.location.lng();
+        console.log(latitude, longitude)
 
-
-
-
-
+        fetchTravelGuide(latitude, longitude)
+      }
+    });
+  }
 
 
 const searchBtn = document.getElementById("btn")
@@ -65,9 +37,27 @@ searchBtn.addEventListener("click", (event) =>{
     event.preventDefault()
     const searchText = document.getElementById("search").value
     console.log(searchText)
-    fetchTravelGuide(searchText);
+
+    GetLatlong(searchText)
 })
 
-// console.log(searchText)
 
+res.map((result) => {
+    const card = document.createElement('div')
+    card.classList.add("card")
+    const name = document.createElement('div')
+    name.classList.add("name")
+    name.textContent = result.name
+    const description = document.createElement('div')
+    description.classList.add("description")
+    description.textContent = result.description
+    const comment = document.createElement('div')
+    comment.classList.add("comment")
+    comment.textContent = result.comment
 
+    card.appendChild(name)
+    card.appendChild(description)
+    card.appendChild(comment)
+    card.appendChild(card)
+
+})
